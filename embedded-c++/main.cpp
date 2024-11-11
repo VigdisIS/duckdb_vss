2d1f251da5e9be4884bcd7025c303b7be93525f6
Vigdis-Irene Steinsund <vigdis.steinsund@hotmail.com>
2024-11-11 11:09:21 +0100

    Add step to print index information on table

--- embedded-c++/main.cpp
+++ embedded-c++/main.cpp
@@ -11,10 +11,12 @@ int main() {
 
 	con.Query("CREATE TABLE my_vector_table (vec FLOAT[3])");
 	con.Query("INSERT INTO my_vector_table SELECT array_value(a,b,c) FROM range(1,10) ra(a), range(1,10) rb(b), range(1,10) rc(c)");
+	// Create centroid HNSW index + cluster HNSW indexes
 	con.Query("CREATE INDEX my_hnsw_index ON my_vector_table USING HNSW (vec)");
+	// Get the index names
+	auto result_duckdb_indexes =  con.Query("SELECT index_name FROM duckdb_indexes()");
+	result_duckdb_indexes->Print();
+	// Perform two-tiered vector similarity search
 	auto result = con.Query("SELECT * FROM my_vector_table ORDER BY array_distance(vec, [9,9,9]::FLOAT[3]) LIMIT 3");
 	result->Print();
-	// con.Query("SELECT * FROM my_vector_table ORDER BY array_distance(vec, [1,2,3]::FLOAT[3]) LIMIT 3");
-	// auto result_duckdb_indexes =  con.Query("SELECT index_name FROM duckdb_indexes()");
-	// result_duckdb_indexes->Print();
 }
