c516ebdbe716805e3dc374c75ce37f46ad803166
Vigdis-Irene Steinsund <vigdis.steinsund@hotmail.com>
2024-11-20 00:37:50 +0100

    Remove redundant for loop and if stmts

--- src/hnsw/hnsw_optimize_scan.cpp
+++ src/hnsw/hnsw_optimize_scan.cpp
@@ -97,8 +97,6 @@ public:
 		auto &duck_table = table.Cast<DuckTableEntry>();
 		auto &table_info = *table.GetStorage().GetDataTableInfo();
 
-		auto &table_indexes = table_info.GetIndexes();
-
 		// Find the index
 		unique_ptr<HNSWIndexScanBindData> bind_data = nullptr;
 		vector<reference<Expression>> bindings;
@@ -156,7 +154,7 @@ public:
 			}
 
 			auto ef_search = hnsw_index.index.expansion_search();
-			auto search_result = hnsw_index.index.ef_search(query_vector.get(), top_n.limit, ef_search);
+			auto search_result = hnsw_index.index.ef_search(query_vector.get(), 1, ef_search);
 
 			auto centroid = search_result[0];
 
@@ -167,60 +165,50 @@ public:
 			return true;
 		});
 
-		for (const auto &outer_index : table_indexes.Indexes()) {
-			if (outer_index->GetIndexName() == std::to_string(key)) {
-				std::cout << "Entered index: " << outer_index->GetIndexName() << std::endl;
-
-				// Capture the name of the current outer_index for comparison inside the lambda.
-				auto outer_index_name = outer_index->GetIndexName();
-
-				table_info.GetIndexes().BindAndScan<HNSWIndex>(context, table_info, [&](HNSWIndex &inner_index) {
-					if (inner_index.GetIndexName() == outer_index_name) {
-						// Confirm the inner_index matches the outer_index by their names.
-						std::cout << "Matching HNSWIndex found: " << inner_index.GetIndexName() << std::endl;
-						// Reset the bindings
-						bindings.clear();
-
-						// Check that the projection expression is a distance function that matches the index
-						if (!inner_index.TryMatchDistanceFunction(projection_expr, bindings)) {
-							return false;
-						}
-						// Check that the HNSW index actually indexes the expression
-						unique_ptr<Expression> index_expr;
-						if (!inner_index.TryBindIndexExpression(get, index_expr)) {
-							return false;
-						}
-
-						auto &const_expr_ref = bindings[1];
-						auto &index_expr_ref = bindings[2];
-
-						if (const_expr_ref.get().type != ExpressionType::VALUE_CONSTANT ||
-						    !index_expr->Equals(index_expr_ref)) {
-							// Swap the bindings and try again
-							std::swap(const_expr_ref, index_expr_ref);
-							if (const_expr_ref.get().type != ExpressionType::VALUE_CONSTANT ||
-							    !index_expr->Equals(index_expr_ref)) {
-								// Nope, not a match, we can't optimize.
-								return false;
-							}
-						}
-
-						const auto vector_size = inner_index.GetVectorSize();
-						const auto &matched_vector = const_expr_ref.get().Cast<BoundConstantExpression>().value;
-						auto query_vector = make_unsafe_uniq_array<float>(vector_size);
-						auto vector_elements = ArrayValue::GetChildren(matched_vector);
-						for (idx_t i = 0; i < vector_size; i++) {
-							query_vector[i] = vector_elements[i].GetValue<float>();
-						}
-
-						bind_data = make_uniq<HNSWIndexScanBindData>(duck_table, inner_index, top_n.limit,
-						                                             std::move(query_vector));
-						return true;
-					}
-					return false;
-				});
+		table_info.GetIndexes().BindAndScan<HNSWIndex>(context, table_info, [&](HNSWIndex &inner_index) {
+			if (inner_index.GetIndexName() != std::to_string(key)) {
+				return false;
 			}
-		}
+ 						std::cout << "Matching HNSWIndex found: " << inner_index.GetIndexName() << std::endl;
+ 						// Reset the bindings
+ 						bindings.clear();
+ 
+ 						// Check that the projection expression is a distance function that matches the index
+ 						if (!inner_index.TryMatchDistanceFunction(projection_expr, bindings)) {
+ 							return false;
+ 						}
+ 						// Check that the HNSW index actually indexes the expression
+ 						unique_ptr<Expression> index_expr;
+ 						if (!inner_index.TryBindIndexExpression(get, index_expr)) {
+ 							return false;
+ 						}
+ 
+ 						auto &const_expr_ref = bindings[1];
+ 						auto &index_expr_ref = bindings[2];
+ 
+ 						if (const_expr_ref.get().type != ExpressionType::VALUE_CONSTANT ||
+ 						    !index_expr->Equals(index_expr_ref)) {
+ 							// Swap the bindings and try again
+ 							std::swap(const_expr_ref, index_expr_ref);
+ 							if (const_expr_ref.get().type != ExpressionType::VALUE_CONSTANT ||
+ 							    !index_expr->Equals(index_expr_ref)) {
+ 								// Nope, not a match, we can't optimize.
+ 								return false;
+ 							}
+ 						}
+ 
+ 						const auto vector_size = inner_index.GetVectorSize();
+ 						const auto &matched_vector = const_expr_ref.get().Cast<BoundConstantExpression>().value;
+ 						auto query_vector = make_unsafe_uniq_array<float>(vector_size);
+ 						auto vector_elements = ArrayValue::GetChildren(matched_vector);
+ 						for (idx_t i = 0; i < vector_size; i++) {
+ 							query_vector[i] = vector_elements[i].GetValue<float>();
+ 						}
+ 
+ 						bind_data = make_uniq<HNSWIndexScanBindData>(duck_table, inner_index, top_n.limit,
+ 						                                             std::move(query_vector));
+			return true;
+		});
 
 		if (!bind_data) {
 			// No index found
