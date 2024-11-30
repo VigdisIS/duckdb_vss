1cb851e99a6322c631c2e392fab80b96c2158746
Vigdis-Irene Steinsund <vigdis.steinsund@hotmail.com>
2024-12-01 00:15:37 +0100

    Update results to include train data index for validation

--- embedded-c++/get_results.cpp
+++ embedded-c++/get_results.cpp
@@ -50,19 +50,17 @@ int GetVectorDimensionality(int tableIndex) {
 }
 
 void InitializeResultsTable(Connection& con, const std::string& table_name, int& vector_dimensionality) {
-    con.Query("CREATE OR REPLACE TABLE results.vss_" + table_name + "_results (dataset_name VARCHAR, top_k INT, test_query_vector_index INT, test_query_vector FLOAT[" + std::to_string(vector_dimensionality) + "], query_result_set VARCHAR);");
+    con.Query("CREATE OR REPLACE TABLE results.vss_" + table_name + "_results (dataset_name VARCHAR, top_k INT, test_query_vector_index INT, test_query_vector FLOAT[" + std::to_string(vector_dimensionality) + "], result_vector_index INT, result_vector FLOAT[" + std::to_string(vector_dimensionality) + "]);");
     con.Query("INSERT INTO results.vss_" + table_name + "_results SELECT * FROM memory." + table_name + "_results;");
     auto res = con.Query("SELECT * FROM results.vss_" + table_name + "_results LIMIT 1;");
     assert(res->RowCount() == 1);
-    // con.Query("CREATE OR REPLACE TABLE results." + table_name + "_results (dataset_name VARCHAR, cluster_amount INT, top_k INT, test_query_vector_index INT, test_query_vector FLOAT[" + std::to_string(vector_dimensionality) + "], cluster_index INT, query_result_set FLOAT[" + std::to_string(vector_dimensionality) + "][100]);");
-    // con.Query("CREATE OR REPLACE TABLE results." + table_name + "_results (dataset_name VARCHAR, cluster_amount INT, top_k INT, test_query_vector_index INT, test_query_vector VARCHAR, cluster_index INT, query_result_set VARCHAR);");
 }
 
 void GetResults() {
     DuckDB db(nullptr);
     Connection con(db);
 
-    for (int tableIndex = 3; tableIndex <= 3; ++tableIndex) {
+    for (int tableIndex = 0; tableIndex <= 3; ++tableIndex) {
         auto table_name = GetTableName(tableIndex);
         std::cout << "Table name: " << table_name << std::endl;
         auto vector_dimensionality = GetVectorDimensionality(tableIndex);
@@ -77,13 +75,16 @@ void GetResults() {
         std::string full_table_name = table_name + "_results";
         std::cout << "Results table: " << full_table_name << std::endl;
 
-        con.Query("CREATE OR REPLACE TABLE " + table_name + "_results (dataset_name VARCHAR, top_k INT, test_query_vector_index INT, test_query_vector FLOAT[" + std::to_string(vector_dimensionality) + "], query_result_set VARCHAR);");
+        con.Query("CREATE OR REPLACE TABLE " + table_name + "_results (dataset_name VARCHAR, top_k INT, test_query_vector_index INT, test_query_vector FLOAT[" + std::to_string(vector_dimensionality) + "],  result_vector_index INT, result_vector FLOAT[" + std::to_string(vector_dimensionality) + "]);");
 
+        auto abb = con.Query("CREATE SEQUENCE " + table_name + "_id_sequence START 1;");
+        abb->Print();
+        auto abbc = con.Query("ALTER TABLE memory." + table_name + "_train ADD COLUMN id INTEGER DEFAULT nextval('" + table_name + "_id_sequence');");
+        abbc->Print();
+        
         Appender appender(con, full_table_name);
 
-        // for (int cluster_amount : cluster_amounts) {
             std::cout << "Creating HNSW index" << std::endl;
-            // auto cluster_amount_string = std::to_string(cluster_amount);
             con.Query("CREATE INDEX hnsw_index ON " + table_name + "_train" + " USING HNSW (vec);");
             auto indexes_count = con.Query("select count(index_name) from duckdb_indexes");
             indexes_count->Print();
@@ -91,35 +92,40 @@ void GetResults() {
             std::cout << "Index created" << std::endl;
 
             for (idx_t i = 0; i < test_vectors->RowCount(); i++) {
-                std::cout << "Running test query " << i << " of " << test_vectors->RowCount() << std::endl;
+                std::cout << "Running test query " << i + 1 << " of " << test_vectors->RowCount() << std::endl;
                 auto test_query_vector = test_vectors->GetValue(0, i);
                 auto test_query_vector_string = test_vectors->GetValue(0, i).ToString();
                 int test_query_vector_index = i;
-                auto test_query_vector_index_string = std::to_string(test_query_vector_index);
 
                 auto result = con.Query("SELECT * FROM " + table_name + "_train" + " ORDER BY array_distance(vec, " + test_query_vector_string + "::FLOAT[" + vec_dim_string + "]) LIMIT 100;");
 
-                std::string result_string = "[";
                 for (idx_t j = 0; j < result->RowCount(); j++) {
-                    auto row = result->GetValue(0, j);
-                    result_string += row.ToString();
-                    // Check if this is not the last iteration
-                    if (j < result->RowCount() - 1) {
-                        result_string += ", ";
+                    auto result_vector = result->GetValue(0, j);
+                    auto result_vector_string = result_vector.ToString();
+                    auto result_vector_row = result->GetValue(1, j);
+                    int result_vector_index_int = result_vector_row.GetValue<int>();
+                    int result_vector_index;
+                    if (result_vector_index_int == 0) {
+                        result_vector_index = 0;
+                    } else {
+                        result_vector_index = result_vector_index_int - 1;
+                        assert(result_vector_index_int - result_vector_index == 1);
                     }
+
+                    appender.AppendRow(Value(table_name), Value::INTEGER(100), Value::INTEGER(test_query_vector_index), Value(test_query_vector), Value::INTEGER(result_vector_index), Value(result_vector));
                 }
-                result_string += "]";
-
-                appender.AppendRow(Value(table_name), Value::INTEGER(100), Value::INTEGER(test_query_vector_index), Value(test_query_vector), Value(result_string));
-
             }
+
+            std::cout << "Finished test queries" << std::endl;
+
             auto indexes = con.Query("select distinct index_name from duckdb_indexes where table_name = '" + table_name + "_train';");
             for (idx_t i = 0; i < indexes->RowCount(); i++) {
                 con.Query("DROP INDEX \"" + indexes->GetValue(0, i).ToString() + "\";");
             }
             auto indexes_none = con.Query("select * from duckdb_indexes;");
+            indexes_none->Print();
             assert(indexes_none->RowCount() == 0);
-        // }
+
         appender.Close();
 
         con.Query("ATTACH 'results.db' AS results;");
@@ -132,16 +138,15 @@ void OutputResultTables() {
     DuckDB db(nullptr);
     Connection con(db);
 
-    con.Query("SET threads = 10;");
-
     con.Query("ATTACH 'results.db' AS results;");
 
-    for (int tableIndex = 3; tableIndex <= 3; ++tableIndex) {
+    for (int tableIndex = 0; tableIndex <= 3; ++tableIndex) {
         auto table_name = GetTableName(tableIndex);
         con.Query("COPY results.vss_" + table_name + "_results TO 'vss_" + table_name + "_results.parquet' (FORMAT PARQUET);");
-        // con.Query("COPY results." + table_name + "_results TO '" + table_name + "_results.csv' (HEADER, DELIMITER ',');");
+        auto check = con.Query("SELECT * from 'vss_" + table_name + "_results.parquet' limit 1;");
+        check->Print();
+        assert(check->RowCount() == 1);
     }
-
     con.Query("DETACH results;");
 }
 
