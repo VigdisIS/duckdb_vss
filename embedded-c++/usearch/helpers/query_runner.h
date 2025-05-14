302f222e71270136d71e77e47651ea7b9790413e
Vigdis-Irene Steinsund <vigdis.steinsund@hotmail.com>
2025-05-14 14:27:42 +0200

    Updare reachable vectors query

--- embedded-c++/usearch/helpers/query_runner.h
+++ embedded-c++/usearch/helpers/query_runner.h
@@ -16,7 +16,7 @@ public:
     static void aggregateBMStats(Connection& con, const std::string& table_name, size_t num_queries, size_t num_del_add);
     static void outputTableAsCSV(Connection& con, const std::string& full_table_name, const std::string& output_file_name);
     static unique_ptr<MaterializedQueryResult> getSampleVectors(Connection& con, const std::string& table_name, int rows = 1);
-    static unique_ptr<MaterializedQueryResult> getSampleReachableVectors(Connection& con, const std::string& table_name, int rows, std::unordered_set<size_t>& available_points);
+    static unique_ptr<MaterializedQueryResult> getSampleReachableVectors(Connection& con, const std::string& table_name, int rows, std::vector<size_t>& available_points);
     static std::vector<unique_ptr<MaterializedQueryResult>> partitionDataset(Connection& con, const std::string& table_name, int num_partitions = 100);
     static void updateTestDataset(Connection& con, const DatasetConfig& dataset, Appender& test_appender, std::vector<std::tuple<int, std::vector<float>, std::vector<size_t>>>& ground_truth_indices, int test_vectors_count);
 };
